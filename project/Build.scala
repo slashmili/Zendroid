db8f8c383661223038b583d6579a53f65ae2c9f7
Milad <eslashmili@gmail.com>
2012-11-13 17:55:23 +0800

    upgrdaed env to scala 2.9.2 and sbt 0.12.1

--- project/Build.scala
+++ project/Build.scala
@@ -6,18 +6,25 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Zenroid",
-    version := "2.25",
-    scalaVersion := "2.9.0-1",
-    platformName in Android := "android-14"
+    version := "2.26",
+    versionCode := 226,
+    scalaVersion := "2.9.2",
+    platformName in Android := "android-16"
+  )
+
+  val proguardSettings = Seq (
+    useProguard in Android := true
   )
 
   lazy val fullAndroidSettings =
     General.settings ++
     AndroidProject.androidSettings ++
     TypedResources.settings ++
+    proguardSettings ++
+    AndroidManifestGenerator.settings ++
     AndroidMarketPublish.settings ++ Seq (
       keyalias in Android := "Zendroid",
-      libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+      libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
     )
 }
 
@@ -28,10 +35,13 @@ object AndroidBuild extends Build {
     settings = General.fullAndroidSettings
   )
 
-/*  lazy val tests = Project (
+  lazy val tests = Project (
     "tests",
     file("tests"),
-    settings = General.settings ++ AndroidTest.androidSettings
+    settings = General.settings ++
+               AndroidTest.androidSettings ++
+               General.proguardSettings ++ Seq (
+      name := "ZendroidTests"
+    )
   ) dependsOn main
-*/
 }
